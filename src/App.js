35a5a675d71f2f5eaaeecb388f2736361466b5de
Carly Ciricillo <carly.ciricillo@wunderkind.co>
2022-11-06 09:24:40 -0500

    Fix linter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import styled from "styled-components";
-
 import TimersView from "./views/TimersView";
 
 const Container = styled.div`
